61d34f7d16131adfeff3067c38f8abd6ce102f1c
Volodymyr Gladun <vgladun@gmail.com>
2019-12-16 01:10:59 +0000

    suppress warning 4505 for MSVC

--- include/detail/Utils.hpp
+++ include/detail/Utils.hpp
@@ -25,9 +25,11 @@ using time_point = high_resolution_clock::time_point;
 #ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable : 4505)  
+#endif
 [[maybe_unused]] static auto time_elapsed(time_point const& start_time) -> float {
   return duration_cast<duration<float>>(time_now() - start_time).count();
 }
+#ifdef _MSC_VER
 #pragma warning(pop)
 #endif
 //*****************************************************************************
